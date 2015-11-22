18f69a433816360a537a7270f1ced50cf86bd99b
IanChar <ianchar4@gmail.com>
2015-11-21 22:20:50 -0700

    Finished game board layout

--- public/javascripts/game_board.js
+++ public/javascripts/game_board.js
@@ -1,6 +1,11 @@
 console.log('Module loaded')
 angular.module('GameBoard', [])
 
+.controller('Header', ['$scope', function($scope) {
+  $scope.score = 1000;
+}])
+
+
 .controller('gameCol', ['$scope', function($scope) {
   $scope.a = 1;
   $scope.b = 2;

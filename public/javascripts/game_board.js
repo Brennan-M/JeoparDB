7eba7439ac78273e9819b29cbf213b992f2bf07e
IanChar <ianchar4@gmail.com>
2015-11-22 10:24:21 -0700

    Got basic animation going

--- public/javascripts/game_board.js
+++ public/javascripts/game_board.js
@@ -1,3 +1,32 @@
+// Animation for Question click
+$(document).ready(function(){
+  $('.qBox').click(function() {
+    var h = $(this).height();
+    var w = $(this). width();
+    var posn = $(this).offset();
+    console.log(posn)
+    $("body").append("<div id='Question' "
+        + "style='background-color: #0000A0; position: fixed; z-index: 1;"
+        + "top: " + (posn['top'].toString() + "px")
+        + "; left: " + (posn['top'].toString() + "px") + "; "
+        + "height: " + h + "; width: " + w +"; "
+        + "border: 1px solid white'"
+        + "></div>");
+    var offset = $('#Question').offset();
+    console.log(offset)
+    $("#Question").animate({height: "100vh",
+        width: "200vh",
+        "top": ("-=" + offset['top'].toString() + "px"),
+        "left": ("-=" + offset['left'].toString() + "px")},
+        "slow", function() {
+          $(this).bind("click", function(){
+            $(this).remove();
+          })
+        });
+  });
+});
+
+
 console.log('Module loaded')
 angular.module('GameBoard', [])
 

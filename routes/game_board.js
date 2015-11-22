64380d0a0efb2574e647fe103776370ed0599ab3
IanChar <ianchar4@gmail.com>
2015-11-21 17:30:18 -0700

    Added page for the game board

--- routes/game_board.js
+++ routes/game_board.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'JeoparDB' });
+  res.render('game_board', {});
 });
 
-module.exports = router;
+module.exports = router; 

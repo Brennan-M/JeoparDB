b451d6c6a1108998b4c56f236fa931153e93d9c9
IanChar <ianchar4@gmail.com>
2015-11-23 11:11:15 -0700

    Can collect data and date error handling

--- public/javascripts/game_board.js
+++ public/javascripts/game_board.js
@@ -1,6 +1,80 @@
+// Constants
+var NUM_COLS = 5;
+
 // Global vars
-var selectedCluster = [undefined, undefined, undefined, undefined, undefined];
-// Handlers
+var selectedCluster = [];
+for (var i = 0; i < NUM_COLS; i++) {
+  selectedCluster.push(undefined);
+}
+
+// ---------------------Helper functions----------------------
+
+// Apply error message for an invalid date
+function applyInvalidDate(col) {
+  var errorMsg = "<p style='color: red; font-size: 14px;' class='errorMsg'"
+      + " id='em" + col.toString() + "'> Invalid date (mm/dd/yyyy)</p>";
+  $('#date' + col.toString()).append(errorMsg);
+}
+
+// Check if a date is correct
+function checkDate(date) {
+  if (date.length == 0) {
+    return true;
+  }
+  if (date.length != 10) {
+    return false;
+  }
+  // This weird if structure accounts for NaN
+  var month = Number(date.substring(0, 2));
+  if (month > 0 && month < 13) {
+    var day = Number(date.substring(3, 5));
+    if (day > 0 && day < 31) {
+      var year = Number(date.substring(6, 10));
+      if (year > 0) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+//Get the info filled out by the user returns false if invalid info
+function getInfoForCol(col) {
+  var toReturn = []; // startDate, endDate, cluster
+  var startDate = $('#startDate' + col.toString()).val();
+  if (!checkDate(startDate)) {
+    applyInvalidDate(col);
+    return false;
+  }
+  var endDate = $('#endDate' + col.toString()).val();
+  if (!checkDate(endDate)) {
+    applyInvalidDate(col);
+    return false;
+  }
+  toReturn.push(startDate);
+  toReturn.push(endDate);
+  if (selectedCluster[col] == undefined) {
+    toReturn.push(undefined);
+  } else {
+    var tag = selectedCluster[col].substring(0,2);
+    if (tag == "mi") {
+      toReturn.push("Miscellaneous");
+    } else if (tag == "wp") {
+      toReturn.push("Word Play");
+    } else if (tag == "ss") {
+      toReturn.push("Social Sciences")
+    } else if (tag == "pc") {
+      toReturn.push("Pop Culture");
+    } else if (tag == "ar") {
+      toReturn.push("Art and Media");
+    } else {
+      toReturn.push("Science");
+    }
+  }
+  return toReturn;
+}
+
+// ------------------- Handlers -----------------------
 $(document).ready(function() {
   // Question click
   $('.qBox').click(function() {
@@ -34,7 +108,22 @@ $(document).ready(function() {
 
   // Submit button clicked
   $('#submitButton').click(function() {
+    $('.errorMsg').remove();
     // Logic to read input and post request here.
+    var dataToSend = [];
+    var invalidData = false;
+    for (var i = 0; i < NUM_COLS; i++) {
+      colInfo = getInfoForCol(i);
+      if (! colInfo) {
+        invalidData = true;
+      } else {
+        dataToSend.push(colInfo);
+      }
+    }
+    console.log(dataToSend);
+    if (invalidData) {
+      return;
+    }
     $('#setup').remove();
     $('.board-table').css('visibility', 'visible')
   });
@@ -73,8 +162,19 @@ $(document).ready(function() {
     }
     selectedCluster[column] = undefined;
   });
+
+  // Button for date Header
+  $('.dateHead').click(function() {
+    var id = $(this).attr('id');
+    var column = Number(id.substring(id.length - 1, id.length));
+    console.log('#em' + column.toString());
+    $('#em' + column.toString()).remove();
+    $('#startDate' + column.toString()).val("");
+    $('#endDate' + column.toString()).val("");
+  });
 });
 
+// ----------------------- Angular JS stuff ---------------
 
 console.log('Module loaded')
 angular.module('GameBoard', [])
